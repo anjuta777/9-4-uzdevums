6e9f82641ce88d8c36f52183ce2489f28295a880
akibalnika <77092253+anjuta777@users.noreply.github.com>
2022-02-24 13:11:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print('Tikai vaseli skaitli')
+a = int(input('Ievadīt sakotneju pakapē'))
+b = int(input('Ievadīt pedēja pakāpe '))
+i = a
+while i <= b:
+    c = pow(2,a)
+    print(c)
+    i += 1
+    a+= 1 
+              
